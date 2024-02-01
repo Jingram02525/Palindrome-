89ce3ae8da93d01098b0c91d66176782239ed010
Jingram02525 <86169152+Jingram02525@users.noreply.github.com>
2024-01-31 21:12:03 -0800

    Create PalindromeChecker.java

---
+++ PalindromeChecker.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a string to check if it's a palindrome:");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("Success! The input '" + input + "' is a palindrome.");
+        } else {
+            System.out.println("No luck this time, try another palindrome!");
+        }
+    }
+
+    private static boolean isPalindrome(String str) {
+        str = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase(); // Remove non-alphanumeric characters and convert to lower case
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
